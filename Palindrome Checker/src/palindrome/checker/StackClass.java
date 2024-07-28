870a53984277492a053d6f69e80f6f42eefcea9e
2k2TharushaBandara <123946740+2k2TharushaBandara@users.noreply.github.com>
2024-07-28 21:46:42 +0530

    Added ShellSort code

--- Palindrome Checker/src/palindrome/checker/StackClass.java
+++ Palindrome Checker/src/palindrome/checker/StackClass.java
@@ -1,35 +1,72 @@
 package palindrome.checker;
 
 public class StackClass {
-    private final char[] stack;
-    private int top;
+   //variables
+   private static int top,stackMaxSize;
+   private int nElems;
+   private static int[] stack;
+   
+   //constructor
+   public StackClass(int stackSize){
+    this.stackMaxSize = stackSize;
+    top = -1;
+    stack = new int[stackMaxSize];
+    nElems = 0;
+}
 
-    public StackClass(int capacity) {
-        stack = new char[capacity];
-        top = -1;
+   
+//Is the stack full?   
+public boolean isFull(){
+    if(top >= stackMaxSize-1){
+        return true;
     }
+   return false; 
+}   
 
-    public void push(char c) {
-        if (top == stack.length - 1) {
-            
-        } else {
-            top++;
-            stack[top] = c;
+//Is the stack empty?
+public boolean isEmpty(){
+    if(top==-1){
+        return true;
+    }
+   return false; 
+}
+
+//What is the TOP value?
+public int peek(){
+    return top;
+}
+
+//Inserting values to the stack
+public void push(int value){
+   if(isFull()){
+       System.out.println("Can't insert. Stack is full!");
+   }
+   else{
+       top++;
+       stack[top] = value;
+   }  
+}   
+
+//Removing values from the stack
+public int pop(){
+    int popValue=0;
+    if(isEmpty()){
+        System.out.println("Can't pop. Stack is empty!");
+    }
+    else{
+        popValue = stack[top];
+        stack[top] = 0;
+        top--;
+    }
+    return popValue;
+}
+
+    //Display the stack
+    public void displayStack(){
+        System.out.println("The Stack:");
+        for(int i=0; i<=top; i++){
+            System.out.print(stack[i]+"  ");
         }
-    }
-
-    public char pop() {
-        if (isEmpty()) {
-            
-            return '\0'; 
-        } else {
-            char poppedElement = stack[top];
-            top--;
-            return poppedElement;
-        }
-    }
-
-    public boolean isEmpty() {
-        return top == -1;
+        System.out.println();
     }
 }
